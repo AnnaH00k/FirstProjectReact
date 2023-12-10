6de9f4dfc4a5e9ff01d13653f4958ba6e9af2838
AnnaH00k <90142429+AnnaH00k@users.noreply.github.com>
2023-12-10 14:59:00 +0100

    challenge Item layout

--- src/screens/ChallengeItem.tsx
+++ src/screens/ChallengeItem.tsx
@@ -2,6 +2,8 @@ import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View, useWindowDi
 import { MaterialIcons } from '@expo/vector-icons';
 import { Check } from 'phosphor-react-native';
 import React from 'react';
+import { useState } from 'react';
+
 
 //navigation
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -13,7 +15,16 @@ type ChallengeItemProps = NativeStackScreenProps<RootStackParamList, 'ChallengeI
 
 const ChallengeItem = ({ route }: ChallengeItemProps) => {
   const { challengeObject } = route.params;
-  
+  const [taskStates, setTaskStates] = useState(challengeObject.tasks.map(() => false));
+  const toggleTaskState = (index: number) => {
+    setTaskStates(prevStates => {
+      const newStates = [...prevStates];
+      newStates[index] = !newStates[index];
+      return newStates;
+    });
+  };
+  const selectedChallengeType = challengeObject.type;
+
 
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
   return (
@@ -22,23 +33,57 @@ const ChallengeItem = ({ route }: ChallengeItemProps) => {
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <MaterialIcons name="arrow-back" size={35} color="#797878" />
         </TouchableOpacity>
-        <Text style={styles.textHeadline}>Challenge Item</Text>
+        <Text style={styles.textHeadline}>{challengeObject.name}</Text>
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <MaterialIcons name="settings" size={35} color="#797878" />
         </TouchableOpacity>
       </View>
 
       <ScrollView>
-        <Text style={styles.textHeadline}>{challengeObject.name}</Text>
-        <Text style={styles.textHeadline}>{challengeObject.description}</Text>
-        <Text style={styles.textHeadline}>{challengeObject.time}</Text>
-        <Text style={styles.textHeadline}>{challengeObject.type}</Text>
+        <View style={styles.infoBox}>
+            <Text style={styles.textDescription}>{challengeObject.description}</Text>
+        </View>
+        <View style={styles.infoBox}>
+            <Text style={styles.timeHeadline}>Time:</Text>
+            <Text style={styles.textDescription}>{challengeObject.time}</Text>
+        </View>
+
+
+        <View style={styles.buttonContainer}>
+                <TouchableOpacity
+                    style={[styles.challengeTypeButton, selectedChallengeType === 'current' && styles.selectedButton]}
+                >
+                    <Text style={[styles.buttonText2, selectedChallengeType === 'current' && styles.buttonText]}>Current</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.challengeTypeButton, selectedChallengeType === 'saved' && styles.selectedButton]}
+                >
+                    <Text style={[styles.buttonText2, selectedChallengeType === 'saved' && styles.buttonText]}>Saved</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style={[styles.challengeTypeButton, selectedChallengeType === 'completed' && styles.selectedButton]}
+                >
+                    <Text style={[styles.buttonText2, selectedChallengeType === 'completed' && styles.buttonText]}>Completed</Text>
+                </TouchableOpacity>
+            </View>
+
         <View style={styles.sideScroll}>
           {challengeObject.tasks.map((task, index) => (
-            <View key={index} style={styles.taskBox}>
-              <Text style={styles.challengeBoxText}>{task.description}</Text>
-              <Check size={30} color="white" weight="fill" />
+          <TouchableOpacity
+          key={index}
+          style={[
+            styles.taskBox,
+            { backgroundColor: taskStates[index] ? '#48C9B0' : '#0D2B3E' },
+          ]}
+          onPress={() => toggleTaskState(index)}
+        >
+            <View style={{ flexDirection: 'row', gap: 8, justifyContent: 'center',alignItems:'center', alignContent:'center',  }}>
+          {taskStates[index] && (
+            <Check size={25} color="white" weight="fill" />
+          )}
+            <Text style={styles.challengeBoxText}>{task.description}</Text>
             </View>
+        </TouchableOpacity>
           ))}
         </View>
       </ScrollView>
@@ -53,12 +98,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#091825',
   },
+  infoBox: {
+    flexDirection: 'row',
+    marginTop: 20,
+    width: '80%',
+    alignSelf: 'center',
+    flexWrap: 'wrap',
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: 'white',
+    padding: 16,
+  },
   textHeadline: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
     textAlign: 'center',
     marginVertical: 10,
+  },
+  textDescription: {
+    color: '#D0D6D6',
+    fontSize: 25,
+    fontStyle: 'italic',
+    textAlign: 'center',
+    marginHorizontal: 10,
+  },
+  timeHeadline: {
+    color: '#D0D6D6',
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginHorizontal: 10,
   },
   topBar: {
     flexDirection: 'row',
@@ -70,8 +140,11 @@ const styles = StyleSheet.create({
   sideScroll: {
     flexDirection: 'row',
     justifyContent: 'space-around',
+    alignSelf: 'center',
     marginTop: 20,
     flexWrap: 'wrap',
+    width: '80%', // Adjust the width based on your preference
+
   },
   taskBox: {
     backgroundColor: '#0D2B3E',
@@ -80,11 +153,48 @@ const styles = StyleSheet.create({
     marginBottom: 16,
     alignItems: 'center',
     justifyContent: 'center',
-    width: '45%', // Adjust the width based on your preference
+    minWidth: 200,
+    width: 'auto', // Adjust the width based on your preference
   },
   challengeBoxText: {
     color: 'white',
-    fontSize: 15,
+    fontSize: 20,
     textAlign: 'center',
   },
+  challengeTypeButton: {
+    padding: 10,
+    marginHorizontal: 10,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#D0D6D6',
+    flex: 1,
+    alignItems: 'center',
+},
+selectedButton: {
+    backgroundColor: '#0D2B3E',
+    color: 'black',
+},
+ 
+selectedText: {
+    color: 'white',
+    fontSize: 25,
+    marginHorizontal: 10,
+    textAlign: 'center',
+},
+
+buttonText: {
+    color: 'white',
+    fontSize: 25,
+},
+buttonText2: {
+    color: 'grey',
+    fontSize: 25,
+},
+buttonContainer: {
+    flexDirection: 'row',
+    width: '80%',
+    alignSelf: 'center',
+    justifyContent: 'space-around',
+    marginTop: 20,
+},
 });
